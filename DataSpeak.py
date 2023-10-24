1c99a4788df635040c10f3c4aca07956e85e030f
Michael Powers <126989418+michaelpowers8@users.noreply.github.com>
2023-10-23 22:37:04 -0500

    Add files via upload

--- DataSpeak.py
+++ DataSpeak.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # DataSpeak
-
-# ## Import Libraries
-
-# In[1]:
-
-
 import os
 from time import time,sleep
 import tensorflow_hub as hub
-import tensorflow_text as text
 import numpy as np
 import pandas as pd
 from random import randint as rand
@@ -26,7 +15,6 @@ import sklearn.metrics as metrics
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
-from wordcloud import WordCloud, STOPWORDS
 from gensim import models
 warnings.filterwarnings("ignore")
 from tqdm.auto import tqdm
@@ -40,16 +28,11 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 from transformers import BertForQuestionAnswering, BertTokenizer
+from IPython.display import clear_output
 from transformers import BertTokenizer, BertForMaskedLM
 from transformers import logging
 import streamlit as st
 logging.set_verbosity_error()
-
-
-# ## Extra Functions
-
-# In[2]:
-
 
 def generate_word2vec_answer(question, corpus, word2vec_model):
     question_tokens = question.lower().split()
@@ -69,10 +52,6 @@ def generate_word2vec_answer(question, corpus, word2vec_model):
     answer = corpus[max_similarity_index]
 
     return answer
-
-
-# In[3]:
-
 
 def answer_bert_question(question,df,models):
     # Load the pre-trained BERT model and tokenizer
@@ -113,10 +92,6 @@ def answer_bert_question(question,df,models):
     answer = tokenizer.decode(answer_tokens)
     return answer
 
-
-# In[4]:
-
-
 def generate_answers(user_question, df, model, tokenizer, n=5):
     # Calculate TF-IDF vectors for questions
     tfidf_vectorizer = TfidfVectorizer()
@@ -140,10 +115,6 @@ def generate_answers(user_question, df, model, tokenizer, n=5):
 
     return final_answers
 
-
-# In[5]:
-
-
 def calculate_perplexity(answers, model, tokenizer):
     tokenized_answers = tokenizer(answers, return_tensors="pt", padding=True, truncation=True)
     print(tokenized_answers['input_ids'].shape)
@@ -154,192 +125,29 @@ def calculate_perplexity(answers, model, tokenizer):
     perplexity = torch.exp(torch.nn.functional.cross_entropy(logits, tokenized_answers["input_ids"]))
     return perplexity.item()
 
-
-# In[6]:
-
-
-def write_new_sentence(sentence):
-    words = sentence.split()
-    rewritten_sentence = []
-    for word in words:
-        if(word.lower()=='it' or word.lower()=='i' or word.lower()=='will' or word.lower()=='a'):
-            rewritten_sentence.append(word)
-        else:
-            synonyms = get_synonyms(word)
-            if synonyms:
-                # Choose a random synonym from the list
-                synonym = synonyms[0]
-                synonym = preserve_tense(word,synonym)
-                rewritten_sentence.append(synonym)
-            else:
-                rewritten_sentence.append(word)
-    return ' '.join(rewritten_sentence)
-
-
-# In[7]:
-
-
-def clean_sloppy_sentence(sloppy_sentence):
-    # Split the sentence into words
-    words = sloppy_sentence.split()
-
-    # Initialize a list to store cleaned words
-    cleaned_words = []
-
-    # Flag to keep track of the first word
-    first_word = True
-
-    # Iterate through the words
-    for word in words:
-        # Check if the word is considered a valid word
-        if is_word(word):
-            # Capitalize the first letter of the cleaned sentence
-            if first_word:
-                word = word[0].upper() + word[1:]
-                first_word = False
-            cleaned_words.append(word)
-
-    # Join the cleaned words to form the cleaned sentence
-    cleaned_sentence = ' '.join(cleaned_words)
-    return cleaned_sentence
-
-
-# In[8]:
-
-
-def rewrite_sentence(sentence):
-    sentence = write_new_sentence(clean_sloppy_sentence(sentence))
-    # Tokenize the sentence into words
-    words = word_tokenize(sentence)
-
-    # Tag the words with their parts of speech
-    tagged_words = pos_tag(words)
-
-    # Initialize a list to store the rewritten words
-    rewritten_words = []
-
-    for word, pos in tagged_words:
-        # Rewrite the word while preserving tense (if applicable)
-        if pos.startswith('V'):  # Verbs
-            rewritten_word = word
-        else:
-            rewritten_word = word  # Keep non-verbs as they are
-        rewritten_words.append(rewritten_word)
-
-    # Join the rewritten words to form the rewritten sentence
-    rewritten_sentence = ' '.join(rewritten_words)
-
-    return rewritten_sentence
-
-
-# ## Load Data
-
-# In[9]:
-
-
 preprocess_url = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
 encoder_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4"
-
-
-# In[10]:
-
 
 data = pd.read_csv('Good_Merged_QA.csv', sep=',', on_bad_lines='skip', encoding='latin-1', engine='python')
 tags = pd.read_csv('Tags.csv', sep=',', on_bad_lines='skip', encoding='latin-1', engine='python')
 
-
-# ## Clean Data
-
-# In[11]:
-
-
 df = data[(data['Score_Q'] >= 0) & (data['Score_A'] >=0)]
 df = df.reset_index(drop=True)
 
-
-# In[12]:
-
-
-
-
-
-# In[13]:
-
-
-
-
-
-# In[14]:
-
-
 stop_list = set(stopwords.words('english'))
-
-
-# ## EDA
-
-# ## Model Training
-
-# In[15]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(df['Body_Q'],df['Body_A'], test_size=0.2)
-
-
-# In[16]:
-
-
 bert_preprocess_model = hub.KerasLayer(preprocess_url)
-
-
-# In[17]:
-
-
 corpus_train = X_train
 tokenized_corpus_train = [sentence.lower().split() for sentence in corpus_train]
-
-
-# In[18]:
-
-
 corpus_test = X_test
 tokenized_corpus_test = [sentence.lower().split() for sentence in corpus_test]
-
-
-# In[ ]:
-
-
 word2vec_model = models.Word2Vec(tokenized_corpus_train, vector_size=250, window=5, min_count=1, sg=0)
-
-
-# In[ ]:
-
-
 word2vec_model.build_vocab(tokenized_corpus_train,progress_per=100)
-
-
-# In[ ]:
-
-
-df['Body_Q'][22]
-
-
-# In[ ]:
-
-
-df['Body_A'][0]
-
-
-# In[ ]:
-
-
 model_name = "bert-base-uncased"
 tokenizer = BertTokenizer.from_pretrained(model_name)
 model = BertForMaskedLM.from_pretrained(model_name)
 
-
-# In[ ]:
-
-
+"""
 while True:
     question = str(input("Ask a question (type 'exit' to quit): "))
     if(question.lower() in ['exit','done','none','goodbye','bye','finished']):
@@ -350,27 +158,13 @@ while True:
     generated_answers = generate_answers(question, df, model, tokenizer, n=5)
     for i, answer in enumerate(generated_answers):
         print(f"Answer {i + 1}: {answer}\n")
-
-
-# In[ ]:
-
+"""
 
 st.header("BERT Question and Answer")
 st.write("""
          Ask any question and I will give you 5 possible answers.
          """)
 
-
-# In[ ]:
-
-
 user_question = st.text_input("Input question here.")
 generated_answers = generate_answers(user_question, df, model, tokenizer, n=5)
 st.write(generated_answers)
-
-Ask a question (type 'exit' to quit): 
-
-
-
-
-
